82a6e88157ad2febc4e34a74dca890fa2867a9c6
kelly kilstein <kilstein.k@gmail.com>
2025-06-17 14:43:23 +0300

    Fixing CR comments

--- MainProject/MainProject/groot.cpp
+++ MainProject/MainProject/groot.cpp
@@ -1,26 +1,25 @@
+#include <cmath>
 #include <iostream>
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
 
-int main()
-{
+int main() {
     double userNum = 0;
     double userNumSqrt = 0;
 
     cout << "Please enter a number: ";
     cin >> userNum;
 
-    if (userNum <= 0)
-    {
-        cout << "The number must be positive" << endl;
+    if (userNum < 0) {
+        cout << "The number must be not-negative" << endl;
         return 0;
     }
 
-    userNumSqrt = std::sqrt(userNum);
+    userNumSqrt = sqrt(userNum);
 
     cout << "The sqrt is: " << userNumSqrt << endl;
 
-    return 0;
+    return 1;
 }
